09cfda2edf9ef31cdecd1fd08c35b556d427d93c
Inom T <i@inomoz.ru>
2021-02-23 03:20:27 +0300

    adjust response method

--- salesloop_linkedin_api/linkedin.py
+++ salesloop_linkedin_api/linkedin.py
@@ -507,7 +507,7 @@ class Linkedin(object):
         data = res.json()
 
         if data and "status" in data and data["status"] != 200:
-            self.logger.info("request failed: {}".format(data["message"]))
+            self.logger.info("request failed: %s", data)
             return {}
 
         company = data["elements"][0]
