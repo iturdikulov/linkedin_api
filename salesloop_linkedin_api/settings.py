19c084a6a67c1174dff964e6ef8c37ed60d5ac75
Inom T <i@inomoz.ru>
2021-09-17 23:38:54 +0300

    refactor strings

--- salesloop_linkedin_api/settings.py
+++ salesloop_linkedin_api/settings.py
@@ -1,5 +1,5 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-LOGIN_TIMEOUT = float(os.getenv('LINKEDIN_API_LOGIN_TIMEOUT', 220))
-REQUEST_TIMEOUT = float(os.getenv('LINKEDIN_API_REQUEST_TIMEOUT', 220))
+LOGIN_TIMEOUT = float(os.getenv("LINKEDIN_API_LOGIN_TIMEOUT", 220))
+REQUEST_TIMEOUT = float(os.getenv("LINKEDIN_API_REQUEST_TIMEOUT", 220))
