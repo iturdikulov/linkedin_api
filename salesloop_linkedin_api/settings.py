ddbdb63b6a2024d9978ca6675791fa0e04b95c2c
Inom T <i@inomoz.ru>
2023-01-24 23:50:12 +0600

    add getting limits logic

--- salesloop_linkedin_api/settings.py
+++ salesloop_linkedin_api/settings.py
@@ -1,4 +1,5 @@
 import os
+from enum import Enum
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 LOGIN_TIMEOUT = float(os.getenv("LINKEDIN_API_LOGIN_TIMEOUT", 220))
@@ -6,6 +7,8 @@ REQUEST_TIMEOUT = float(os.getenv("LINKEDIN_API_REQUEST_TIMEOUT", 220))
 
 # statistics TTL 1 month, stored in redis
 STATISTICS_TTL = int(os.getenv("LINKEDIN_API_STATISTICS_TTL", 2592000))
+
+OLD_ACCOUNT_MIN_CONNECTIONS = 5000
 
 # Based on https://linkedin.api-docs.io/v1.0
 REQUESTS_TYPES = {
@@ -78,3 +81,50 @@ REQUESTS_TYPES = {
     ),
     "uas": ("uas/authenticate",),
 }
+
+# Maximum number of requests per day
+# we use these multiples to calculate the requests limits
+# 1. New account
+# 2. Account with 5000 connections
+# 3. Account with 5000 connections and premium subscription
+BASE_REQUESTS_LIMITS = {
+    "growth": (1, 1, 1),
+    "jobs": (1, 1, 1),
+    "relationships": (1, 1, 1),
+    "companies": (1, 1, 1),
+    "search": (1, 1, 1),
+    "feed": (1, 1, 1),
+    "messaging": (1, 1, 1),
+    "identity": (1, 1, 1),
+    "other": (1, 1, 1),
+    "uas": (1, 1, 1),
+}
+
+def get_account_requests_limits(connections_number, is_premium):
+    """
+    Get account requests limits based on connections_number and is_premium
+    Args:
+        connections_number:
+        is_premium:
+
+    Returns: dict - requests limits per request type
+
+    """
+
+    new = 0
+    old = 1  # OLD_ACCOUNT_MIN_CONNECTIONS
+    old_premium = 2  # OLD_ACCOUNT_MIN_CONNECTIONS + premium
+
+    account_requests_limits = {}
+
+    for request_type, limits in BASE_REQUESTS_LIMITS.items():
+        if connections_number < OLD_ACCOUNT_MIN_CONNECTIONS:
+            account_requests_limits[request_type] = limits[new]
+        elif is_premium:
+            account_requests_limits[request_type] = limits[old_premium]
+        else:
+            account_requests_limits[request_type] = limits[old]
+
+    return account_requests_limits
+
+
